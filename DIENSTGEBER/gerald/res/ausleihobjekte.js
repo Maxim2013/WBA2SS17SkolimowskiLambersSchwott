dd380933f8286b3b262900f03d1b04cc79dab375
grotelambers <grotelambers@uni.de>
2017-07-10 12:37:05 +0200

    ..

---
+++ DIENSTGEBER/gerald/res/ausleihobjekte.js
@@ -0,0 +1,25 @@
+{
+	"ausleihobjekt": [
+
+	{
+		"art": 		"buch",
+		"titel":	"titel1",
+		"autor":	"autor1",
+		"isbn":		"1",
+	},
+
+	{
+		"art": 		"buch",
+		"titel":	"titel2",
+		"autor":	"autor2",
+		"isbn":		"2",
+	},
+
+	{	"art": 		"cd",
+		"titel":	"titel1",
+		"autor":	"autor1",
+		"isbn":		"1",
+	},
+
+	]
+}
